33e02056de94288f18f5f976f759bdf2a06038b1
M. AsadUllah <68208088+m-asadullah@users.noreply.github.com>
2024-01-03 09:21:04 -0700

    commit

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = `pdfile-v1`;
+
+// Use the install event to pre-cache all initial resources.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll([
+            '/',
+            'assets/js/script.js',
+            'assets/css/style.css'
+        ]);
+    })());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request).then(res => {
+            const resClone = res.clone();
+            caches.open(CACHE_NAME).then(cache => {
+                // Only cache requests that start with http
+                if (event.request.url.startsWith('http')) {
+                    cache.put(event.request, resClone);
+                }
+            });
+            return res;
+        }).catch(err => caches.match(event.request).then(res => res))
+    );
+});
